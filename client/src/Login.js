3bb75e8b112d2de6411ab2842d984372511a1b86
Shrinjay <shrinjaymukherjee@gmail.com>
2021-02-07 04:11:00 -0700

    Removed unneeded files

--- client/src/Login.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import LoginPage from './loginexport';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<LoginPage />, document.getElementById('login'));
-registerServiceWorker();
